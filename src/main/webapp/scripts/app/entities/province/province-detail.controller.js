c5446e25f6758bc840e66d5d4b4a6ed1485be633
José María Berná Chamber <jbernach@gmail.com>
2016-02-06 14:12:15 +0100

    Added markdown edition and several bugfixes.

--- src/main/webapp/scripts/app/entities/province/province-detail.controller.js
+++ src/main/webapp/scripts/app/entities/province/province-detail.controller.js
@@ -3,6 +3,7 @@
 angular.module('backendApp')
     .controller('ProvinceDetailController', function ($scope, $rootScope, $stateParams, DataUtils, entity, Province) {
         $scope.province = entity;
+
         $scope.load = function (id) {
             Province.get({id: id}, function(result) {
                 $scope.province = result;
